87a908b576572125f1cf02862c795d90b3a4fa32
waltinhocalegari <waltinhocalegari@hotmail.com>
2020-07-28 21:00:22 -0300

    ll

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -4,6 +4,7 @@ import { app } from './app';
 
 const start = async () => {
   console.log('Starting...');
+  console.log('mais ta');
   if (!process.env.JWT_KEY) {
     throw new Error('JWT_KEY must be defined');
   }
